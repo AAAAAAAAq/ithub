c7a14b70a1c4667501efe114384f40e1f5bb0f03
AAAAAAAq <747590770@qq.com>
2018-06-30 18:01:22 +0800

    配置静态资源

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 //app.js 程序的入口
 //功能 负责：配置，监听端口
 const express = require('express');
+// 导入处理模板的模块
+const expressArtTemplate = require('express-art-template');
+const bodyParser = require('body-parser');
+// 导入路由模块
 const router = require('./routes/router');
 const app = express();
 // 监听端口
@@ -9,6 +13,11 @@ const PORT = 3000;
 app.listen(PORT,()=>{
   console.log('监听' + PORT);
 })
-
+// 处理静态资源
+app.use('/public',express.static('./public'))
+app.use('/node_modules', express.static('./node_modules'));
+// 配置模板引擎
+app.engine('html',expressArtTemplate)
+app.use(bodyParser.urlencoded({ extended: false }));
 // 注册路由
 app.use(router);
